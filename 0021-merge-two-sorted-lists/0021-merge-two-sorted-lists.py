23f0de9ae9c35aba47deb1761a089dddbb5aacaf
SWATHI LAKSHMI P <swathilakshmi456@gmail.com>
2026-09-26 11:48:42 +0530

    Time: 1 ms (18.27%), Space: 19.2 MB (94.84%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:   
+        if not l1 or not l2:
+            return l1 or l2
+        
+        if l1.val <= l2.val: #1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else: #2
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
